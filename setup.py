1fc214868363baa2aa1c021642df47ef098de32e
Ross Crawford-d'Heureuse <sendrossemail+github@gmail.com>
2014-02-10 14:33:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='hellosign',
-    version='0.1.0',
+    version='0.1.1',
     author="Ross Crawford-d'Heureuse",
     author_email='sendrossemail@gmail.com',
     packages=['hellosign'],
