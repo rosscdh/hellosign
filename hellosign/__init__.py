ddeb5061ec4f26b75a2e510c214f527812df63ef
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-03 13:03:28 +0100

    fixing import errors

--- hellosign/__init__.py
+++ hellosign/__init__.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-__version_info__ = ('0', '1', '0')
-__version__ = '.'.join(__version_info__)
-
 from .api import BaseApiClient
 from .hellosign import (HelloSign,
                         HelloSignSignature,
                         HelloSignEmbeddedDocumentSignature,
                         HelloSignEmbeddedDocumentSigningUrl)
 from .hello_objects import HelloSigner, HelloDoc
-
-
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 from setuptools import find_packages
+from hellosign.version import __version__
 
 setup(
     name='hellosign',
-    version='0.1.1',
+    packages=['hellosign'],
+    version=__version__,
     author="Ross Crawford-d'Heureuse",
     author_email='sendrossemail@gmail.com',
-    packages=['hellosign'],
     include_package_data=True,
     url='https://github.com/rosscdh/hellosign',
     download_url='https://github.com/rosscdh/hellosign/archive/0.1.1.tar.gz',
     description='Module for interacting with the HelloSign service',
-    long_description=open('README.md').read(),
     zip_safe=False,
     install_requires=[
         'WTForms',
