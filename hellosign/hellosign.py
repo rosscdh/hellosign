ephemerallabs/HelloSignApi

adding in method for creating a signature request from a template

hellosign/hellosign.py
@@ -24,7 +24,6 @@ class HelloSignSignature(HelloSign):
         super(HelloSignSignature, self).__init__(*args, **kwargs)
 
     def add_signer(self, signer):
-        """ Simple dict of {'name': 'John Doe', 'email': 'name@example.com'}"""
         if isinstance(signer, HelloSigner) and signer.validate():
             self.signers.append(signer)
         else:
@@ -34,7 +33,6 @@ class HelloSignSignature(HelloSign):
                 raise Exception("add_signer signer must be an instance of class HelloSigner")
 
     def add_doc(self, doc):
-        """ Simple dict of {'name': '@filename.pdf'}"""
         if isinstance(doc, HelloDoc) and doc.validate():
             self.docs.append(doc)
         else:
@@ -43,19 +41,25 @@ class HelloSignSignature(HelloSign):
             else:
                 raise Exception("add_doc doc must be an instance of class HelloDoc")
 
-    def validate(self):
+    def validate_signers(self):
         if len(self.signers) == 0:
             raise AttributeError('You need to specify at least 1 person as a signer')
+
+    def validate_docs(self):
         if len(self.docs) == 0:
             raise AttributeError('You need to specify at least 1 document')
 
-    def data(self):
+    def signer_data(self):
         data = {}
 
-        for i,signer in enumerate(self.signers):
-            data['signers[%d][name]' % (i,)] = signer.data['name']
-            data['signers[%d][email_address]' % (i,)] = signer.data['email']
-            
+        for i, signer in enumerate(self.signers):
+            if signer.data['role']:
+                data['signers[%s][name]' % (signer.data['role'],)] = signer.data['name']
+                data['signers[%s][email_address]' % (signer.data['role'],)] = signer.data['email']
+            else:
+                data['signers[%d][name]' % (i,)] = signer.data['name']
+                data['signers[%d][email_address]' % (i,)] = signer.data['email']
+
         # Append the initial params
         data.update(self.params)
 
@@ -79,6 +83,24 @@ class HelloSignSignature(HelloSign):
             auth = kwargs['auth']
             del(kwargs['auth'])
 
-        self.validate()
+        self.validate_signers()
+        self.validate_docs()
 
-        return self.signature_request.send.post(auth=auth, data=self.data(), files=self.files(), **kwargs)
+        return self.signature_request.send.post(auth=auth, data=self.signer_data(), files=self.files(), **kwargs)
+
+    def create_from_template(self, template_id, custom_fields=None, *args, **kwargs):
+        auth=None
+        if 'auth' in kwargs:
+            auth = kwargs['auth']
+            del(kwargs['auth'])
+
+        self.validate_signers()
+
+        data = self.signer_data()
+        data['reusable_form_id'] = template_id
+
+        if custom_fields:
+            for k, v in custom_fields.items():
+                data['custom_fields[%s]' % k] = v
+
+        return self.signature_request.send_with_reusable_form.post(auth=auth, data=data, **kwargs)

rosscdh/hellosign

Conflicts:
	hellosign/hello_objects.py
	hellosign/hellosign.py

hellosign/hellosign.py
@@ -32,7 +32,7 @@ class HelloSignSignature(HelloSign):
             self.signers.append(signer)
         else:
             if not signer.validate():
-                raise Exception("HelloSigner Errors %s" % (signers.errors,))
+                raise Exception("HelloSigner Errors %s" % (signer.errors,))
             else:
                 raise Exception("add_signer signer must be an instance of class HelloSigner")
 
@@ -90,9 +90,27 @@ class HelloSignSignature(HelloSign):
             auth = kwargs['auth']
             del(kwargs['auth'])
 
-        self.validate()
+        self.validate_signers()
+        self.validate_docs()
 
         return self.signature_request.send.post(auth=auth, data=self.data(), files=self.files(), **kwargs)
+
+    def create_from_template(self, template_id, custom_fields=None, *args, **kwargs):
+        auth=None
+        if 'auth' in kwargs:
+            auth = kwargs['auth']
+            del(kwargs['auth'])
+
+        self.validate_signers()
+
+        data = self.data()
+        data['reusable_form_id'] = template_id
+
+        if custom_fields:
+            for k, v in custom_fields.items():
+                data['custom_fields[%s]' % k] = v
+
+        return self.signature_request.send_with_reusable_form.post(auth=auth, data=data, **kwargs)
 
 
 class HelloSignEmbeddedDocumentSignature(HelloSignSignature):